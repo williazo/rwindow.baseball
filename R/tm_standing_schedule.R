880783e6c4d91e42b5694d8425432f326d2d1051
Justin Williams <williazo@ucla.edu>
2017-12-10 22:52:08 -0800

    return the object as a data.frame and suppressed the call in the error message.

--- R/tm_standing_schedule.R
+++ R/tm_standing_schedule.R
@@ -16,7 +16,7 @@ tm_standings_schedule <- function(team, year = as.numeric(format(Sys.Date(), "%Y
   if(is.numeric(year) == F){
     stop("year must be specified as a numeric value", call. = F)
   } else if (year> as.numeric(format(Sys.Date(), "%Y"))){
-    stop("year is misspecified. Cannot be greater than the current year")
+    stop("year is misspecified. Cannot be greater than the current year", call. = F)
   } else if(year < 2000){
     stop("year is too far back. Only pulling from the year 2000.")
   }
@@ -29,5 +29,6 @@ tm_standings_schedule <- function(team, year = as.numeric(format(Sys.Date(), "%Y
   tables <- rvest::html_nodes(html_page, "table")
   game_table <- rvest::html_table(tables)
 
+  as.data.frame(game_table)
 
 }
