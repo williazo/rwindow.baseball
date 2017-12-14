6bcc87a10f0aa442fd5a5d7b25e30e541625f636
Justin Williams <williazo@ucla.edu>
2017-12-14 09:19:33 -0800

    edited this so that it is now functional. still having issues because the page is cutoff at 30 values

--- R/tm_plyr_batting.R
+++ R/tm_plyr_batting.R
@@ -12,6 +12,7 @@
 tm_plyr_batting <- function(team, year, min_pa = 0, start_year = NULL, end_year = NULL){
   team_info <- team_specific_fill(team)
   data("MLB_colors")
+  fg_numeric <- MLB_colors[MLB_colors$team==team, "fg_id"]
 
   lg_abrv <- unlist(lapply(regmatches(team_info[[1]],
                                       gregexpr("[[:upper:]]", team_info[[1]])), function(n) paste0(n, collapse = "")))
@@ -23,5 +24,20 @@ tm_plyr_batting <- function(team, year, min_pa = 0, start_year = NULL, end_year 
 
   #Pulling data from fangraphs
   base_url <- "https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&"
-  url <- paste0(base_url,"lg=", lg,"&type=8&season=", end_year, "&month=0&season1=", start_year, "&ind=0&team=", tm_numeric, "&rost=&age=&filter=&players=")
-}
+  url <- paste0(base_url,"lg=", lg, "&qual=", min_pa,"&type=8&season=", end_year, "&month=0&season1=", start_year, "&ind=0&team=", fg_numeric, "&rost=0&age=0&filter=&players=0")
+  html_page <- xml2::read_html(url)
+  url_nodes <- rvest::html_nodes(html_page, "table")
+  team_tbl <- rvest::html_table(url_nodes[[12]], fill = T, header = NA)
+
+  #extracting and cleaning the column names
+  col_names <- team_tbl[2, ]
+  col_names <- gsub("%", "_pct", col_names)
+  col_names <- gsub("#", "n", col_names)
+  col_names <- gsub("\\+", "_plus", col_names)
+
+  #removing the extra top messy part
+  team_tbl <- team_tbl[4:nrow(team_tbl),]
+  names(team_tbl) <- col_names
+
+  return(team_tbl)
+  }
