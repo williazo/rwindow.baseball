2b0e1624f4bc38c8660ffe70d220bcdcaf437325
Justin Williams <williazo@ucla.edu>
2018-05-22 12:25:47 -0700

    Added new function to pull in prospect data from the baseball cube website. Currently it is only functional with single year value pulling the top 100 list.

---
+++ R/prospect_cube.R
@@ -0,0 +1,98 @@
+#'Pull Prospect Rankings
+#'
+#'Scrape data from \url{https://www.thebaseball.cube} to look at the top 100 prospects list for each team or the top prospects by team.
+#'Can use this information to approximate the relative strength of each teams farm system.
+#'
+#' @param team Specify the team abbreviation as character value. Use this value to pull the top prospects by team
+#' @param year Numeric year. Specifying the year will pull only the top 100 prospects. Default is to take the past full season if pulled in the offseason or the current season if pulled after opening day.
+#' @param start_year Numeric value that identifies the beginning year to pull a range of data for the team of interest. This is an optional parameter.
+#' @param end_year Numeric value that identifies the ending year to pull a range of data for the team of interest. This is an optional parameter.
+#' @param src Character value specifying the source to use for the prospect rankings. Options are `"BA"` for Baseball America or `"PIPE"` for MLB Pipeline. Default is Baseball America
+#'
+#' @import xml2
+#' @import rvest
+#'
+#' @examples #pulling top 100 prospectes from 2017
+#' 2017_prospects <- prospect_cube(year = 2017, src = "PIPE")
+#' table(2017_prospects$current_lvl)
+#'
+#' @export
+
+prospect_cube <- function(team, year, src = "BA", start_year = NULL, end_year = NULL){
+
+  #checking to make sure all three parameters are not specified
+  if(missing(year) == FALSE & is.null(start_year) == FALSE & is.null(end_year) == FALSE){
+    warning("Should not specify year with both start_year and end_year. Only pulling the range specified by start_year and end_year", call. = FALSE)
+    year <- NULL
+  }
+
+  #checking to make sure only year or team is specified
+  if(missing(year)==FALSE & missing(team)==FALSE){
+    stop("Must specify only one of team or year", call. = F)
+  }
+
+  #will need to update this each season since opening day changes every year.
+  opening_day <- as.Date("03-29-2018", format = "%m-%d-%Y")
+  #setting the default year based on when the data is being querried.
+  if(missing(year)){
+    if(((opening_day - Sys.Date()) >= 0) == T){
+      year = as.numeric(format(opening_day, "%Y")) - 1
+    } else{
+      year = as.numeric(format(Sys.Date(), "%Y"))
+    }
+  }
+
+
+  #using the team_specific_fill function as a way to check that this variable is properly specified.
+  if(missing(team)==FALSE){
+    team_info <- team_specific_fill(team)
+    fg_numeric <- rwindow.baseball::MLB_colors[rwindow.baseball::MLB_colors$team==team, "fg_id"]
+
+    lg_abrv <- unlist(lapply(regmatches(team_info[[1]],
+                                      gregexpr("[[:upper:]]", team_info[[1]])), function(n) paste0(n, collapse = "")))
+    lg <- tolower(lg_abrv)
+    }
+
+  #code for checking that start_year and end_year are properly specified
+  if(is.null(start_year) == T & is.null(end_year) == T){
+    start_year = year; end_year = year
+    #if not specified then use year value for both values
+  } else if((is.null(start_year) == F & is.null(end_year) == T) | (is.null(start_year) == T & is.null(end_year) == F)){
+    stop("to specify a range start_year and end_year must both be entered", call. = F)
+    #only specified one of the two values
+  } else if(is.null(start_year) == F & is.null(end_year) == F){
+    #both are specified
+    if(is.numeric(start_year) == F | is.numeric(end_year) == F){
+      stop("Both start_year and end_year must be numeric", call. = F)
+      #stopping if they are both not numeric values
+    } else if(is.numeric(start_year) == T & is.numeric(end_year) == T){
+      #checking again to make sure they are both numeric
+      if(start_year>end_year){
+        stop("Improperly specified range. start_year must be less than or equal to end_year", call. = F)
+        #making sure that the start year is always lower than the end year
+      }
+    }
+  }
+
+  #Pulling data from the baseballcube
+  base_url <- "http://www.thebaseballcube.com/prospects/byYear.asp?Y="
+  url <- paste0(base_url, year, "&Src=", src)
+  html_page <- xml2::read_html(url)
+  url_nodes <- rvest::html_nodes(html_page, "table")
+  top_tbl <- rvest::html_table(url_nodes[[15]], fill = T, header = T)
+  #splitting the batting and throwing handedness into two separate variables
+  top_tbl$bat_hand <- substr(top_tbl$`B-T`, start = 1, stop = 1)
+  top_tbl$throw_hand <- substr(top_tbl$`B-T`, start = 3, stop = 3)
+  #removing the combined variable
+  top_tbl <- top_tbl[,-which(names(top_tbl)=="B-T")]
+
+  #extracting and cleaning the column names
+  names(top_tbl) <- gsub("\\s", "_", names(top_tbl))
+  #splitting the current organization and current level into two separate variables
+  top_tbl$current_org <- gsub("\\s*\\([^\\)]+\\)$", "", top_tbl$`Current_Org_(Level)`)
+  top_tbl$current_lvl <- gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.", "", top_tbl$`Current_Org_(Level)`, perl=T)
+  #removing the combined variable
+  top_tbl <- top_tbl[,-which(names(top_tbl)=="Current_Org_(Level)")]
+
+  return(top_tbl)
+}
